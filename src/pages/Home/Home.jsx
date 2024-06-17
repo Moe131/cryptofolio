5e1a0afbbbbeea3728988d3d822aceff3eb29ff2
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-17 00:40:50 -0700

    home page search bar created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,17 @@ import "./Home.css"
 
 function Home(){
     return (
-        <div>
-            Home
+        <div className="home">
+            <div className="hero">
+                <h1>Largest <br/> Crypto Coins by Market Capitalization</h1>
+                <p>Welcome to the world's largest cryptocurrency marketplace.
+                    Sign up to explore more about cryptos. 
+                </p>
+                <form>
+                    <input type="text" placeholder="Search Coin" />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
         </div>
     )
 }
