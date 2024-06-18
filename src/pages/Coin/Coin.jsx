fc1bc90c0f819f9301a03ad2b76e05716c34ffde
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-18 13:49:15 -0700

    coin page loading added

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,12 +1,46 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "./Coin.css"
 
 function Coin(){
-    return (
-        <div>
-            <h1>Coins</h1>
-        </div>
-    )
+    const {coinId} = useParams()
+    const [coinData, setCoinData] = React.useState()
+
+    async function fetchCoin(){
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              'x-cg-demo-api-key': 'CG-r6niR1noVmBu5S5vAvxheV6U	'
+            }
+          };
+          
+        await fetch('https://api.coingecko.com/api/v3/coins/'+coinId, options)
+            .then(response => response.json())
+            .then(response => setCoinData(response))
+            .catch(err => console.error(err));
+    }
+    React.useEffect(() => { fetchCoin() },[])
+    if (coinData) {
+        return (
+            <div className="coin">
+                <div className="coin-name">
+                    <img src={coinData.image.large} />
+                    <p><b>{coinData.name}</b></p>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="loading">
+                <div className="spin">
+                    
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default Coin
