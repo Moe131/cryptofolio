d52e803c2d7ba2f2f26db8511a485b130df6a1b9
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-23 23:55:38 -0700

    aws amplify initialized

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cryptofolio": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "cryptofolioeb46cdc7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3bf55b77f": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,23 @@ import Login from "./pages/Login/Login";
 
 
 function App() {
+  const [isAuthenticated, setAuthenticated] = React.useState(flase)
+  function updateAuth(newVal) {
+    setAuthenticated(newVal)
+  }
+
   return (
     <div className="app">
       <BrowserRouter>
       <Navbar />
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={<Home isAuthenticated={isAuthenticated} />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/coin/:coinId" element={<Coin />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp  isAuthenticated={isAuthenticated} updateAuth={updateAuth}  />} />
+          <Route path="/login" element={<Login isAuthenticated={isAuthenticated} updateAuth={updateAuth}  />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
         <Footer />
